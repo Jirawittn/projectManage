bbc44beb3cb69f1ac6719e45e0ad5436ee7a2af5
jirawittn <jirawitth@kkumail.com>
2022-11-11 09:31:12 +0700

    Master

---
+++ models/manages.js
@@ -0,0 +1,42 @@
+//ใช้งาน mongoose
+const mongoose = require('mongoose')
+const mongo = require('mongodb')
+// const dbUrl='mongodb://localhost:27017/myProject' ||  process.env.DB_URL
+const dbUrl = 'mongodb+srv://mek:1234@cluster0.zn9r8eb.mongodb.net/?retryWrites=true&w=majority'
+
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).catch(err=>console.log(err))
+
+const db = mongoose.connection
+const Schema = mongoose.Schema
+
+const manageSchema = new Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    price:{
+        type:Number,
+        require:true
+    },
+    number:{
+        type:Number,
+        require:true
+    },
+    dangerNumber:{
+        type:Number,
+        require:true
+    },
+    safeNumber:{
+        type:Number,
+        require:true
+    },
+}, { timestamps:true }
+)
+
+const Manages = module.exports = mongoose.model("storages", manageSchema)
+
+
